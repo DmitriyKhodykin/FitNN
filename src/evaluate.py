43fc23c937ca3a11fd889b6d42cb6aacc26a4ba3
Dmitry Khodykin <mail@hodykin.ru>
2022-01-10 11:42:22 +0300

    feat(experiments): Add log file

---
+++ src/evaluate.py
@@ -0,0 +1,24 @@
+import json
+from sklearn import datasets
+
+from neural_network import NeuralNetwork
+
+
+if __name__ == '__main__':
+    # Dataset
+    iris = datasets.load_iris()
+    data = [
+        (
+            iris.data[i][None, ...],
+            iris.target[i]
+        ) for i in range(
+            len(iris.target)
+        )
+    ]
+    # NN Instance
+    neural_network = NeuralNetwork(data)
+    neural_network.train()
+    accuracy = neural_network.calc_accuracy()
+
+    with open("log.csv", "w") as fd:
+        json.dump({"accuracy": accuracy}, fd, indent=4)
